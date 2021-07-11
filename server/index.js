3bf71de83719353bd546964d2c5ebca5f6dda2d8
Santaphobia <72969670+Santaphobia@users.noreply.github.com>
2021-07-11 21:57:00 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,40 +30,6 @@ app.use(express.static(__dirname + '/../public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// БД
-
-// Пользователи
-// id
-// name
-// password
-// avatar_path
-// status
-
-// Созданные чаты
-// id
-// name
-// user_id
-// is_private
-
-// Соответствие чатов и пользователей
-// chat_id
-// user_id
-
-// Сообещения
-// id
-// chat_id
-// user_id
-// text
-// date_create
-// is_read
-
-// const Sequelize = require("sequelize");
-// const sequelize = new Sequelize("usersdb2", "root", "123456", {
-//     dialect: "mysql",
-//     host: "localhost"
-// });
-
-// const db = require('./models/index');
 
 
 app.get('/api/v1/chat', (req, res) => {
